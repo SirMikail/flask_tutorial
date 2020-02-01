80e49d2ce3e528d4c4ba6b61e087c2ea682afc73
SirMikail <mikail_macatangay@yahoo.com>
2020-02-01 09:01:23 +0800

    test

--- hotdog.py
+++ hotdog.py
@@ -1,14 +1,1 @@
-<<<<<<< HEAD
-print("WALANG MAGEEDDDIIIIT HAHAHAH nauna ako")
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-print("ANG LAMIG")
-=======
-print("HELLOOOOOOOOOOOOOOO WAYV")
->>>>>>> 0d23bcfcb0498591212171c4be0b5ec71427359a
-=======
-print("HAHAAHAHAHAHAHAHAHHAA DREAMCATCHER BEST GROUP!)
->>>>>>> a2536a4d867f635a8a21281d62b12d887fa519a5
-print("Stan Everglow")
->>>>>>> 103bb01367fb6e59c767fda5b83c63234d7abb17
+print("Ayos tayo")
